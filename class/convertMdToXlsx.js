acc85a7f49c1fc575e5308c99611f0dd6c0fdf63
user <user@dip155.barnardsoft.local>
2020-04-02 15:37:24 +0900

    remove unnecessary comments

--- class/convertMdToXlsx.js
+++ class/convertMdToXlsx.js
@@ -10,11 +10,6 @@ class ConvertMdToXlsx extends ConvertMd {
             console.log('start converting [' + readFile + '] to [' + writeFile + ']...');
             var workbook = new Excel.Workbook();
     
-            /*workbook.creator = "Me";
-            workbook.lastModifiedBy = "Her";
-            workbook.created = new Date(1985, 8, 30);
-            workbook.modified = new Date();*/
-    
             var sheet1 = workbook.addWorksheet('入力チェック');
             var sheet2 = workbook.addWorksheet('表示チェック');
             var sheet3 = workbook.addWorksheet('画面制御');
